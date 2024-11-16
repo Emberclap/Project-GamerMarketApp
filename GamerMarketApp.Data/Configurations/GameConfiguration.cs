2d0a2abbcd84bf5055d749da822844aaf5604398
Emberclap <133802297+Emberclap@users.noreply.github.com>
2024-11-16 21:04:58 +0200

    Add Sandbox Genre and fix some other genre names

--- GamerMarketApp.Data/Configurations/GameConfiguration.cs
+++ GamerMarketApp.Data/Configurations/GameConfiguration.cs
@@ -15,15 +15,16 @@ namespace GamerMarketApp.Data.Configurations
                  new Genre { Id = 3, Name = "Fighting" },
                  new Genre { Id = 4, Name = "Sports" },
                  new Genre { Id = 5, Name = "Racing" },
-                 new Genre { Id = 6, Name = "Role-playing game" },
-                 new Genre { Id = 7, Name = "Survival horror" },
-                 new Genre { Id = 8, Name = "Massively multiplayer online game" },
-                 new Genre { Id = 9, Name = "Multiplayer online battle Arena" },
-                 new Genre { Id = 10, Name = "First-person shooter" },
+                 new Genre { Id = 6, Name = "Role-Playing game" },
+                 new Genre { Id = 7, Name = "Survival" },
+                 new Genre { Id = 8, Name = "MMORPG" },
+                 new Genre { Id = 9, Name = "MOBA" },
+                 new Genre { Id = 10, Name = "First-Person Shooter" },
                  new Genre { Id = 11, Name = "Shooter" },
                  new Genre { Id = 12, Name = "Strategy" },
-                 new Genre { Id = 13, Name = "Music video game" },
-                 new Genre { Id = 13, Name = "Battle royale" });
+                 new Genre { Id = 13, Name = "Music Video game" },
+                 new Genre { Id = 14, Name = "Battle royale" },
+                 new Genre { Id = 15, Name = "Sandbox" });
         }
     }
 }
