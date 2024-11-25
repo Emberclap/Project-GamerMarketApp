815c05ae4e80364a8eb09bedb7700916a4efd3b7
Emberclap <133802297+Emberclap@users.noreply.github.com>
2024-11-26 00:21:32 +0200

    Added DropdownService to help fill the Add/Edit forms without adding more than 1 Repository in Game and ItemService Layers

---
+++ GamerMarketApp.Services.Data/DropdownService.cs
@@ -0,0 +1,33 @@
+﻿using GamerMarketApp.Data.Models;
+using GamerMarketApp.Data.Repository.Interfaces;
+using GamerMarketApp.Services.Data.Interfaces;
+
+namespace GamerMarketApp.Services.Data
+{
+    public class DropdownService(IGenericRepository<ItemSubtype> itemSubtypeRepository,
+        IGenericRepository<Game> gameRepository,
+        IGenericRepository<Genre> genreRepository) : IDropdownService
+    {
+        private readonly IGenericRepository<ItemSubtype> itemSubtypeRepository = itemSubtypeRepository;
+        private readonly IGenericRepository<Game> gameRepository = gameRepository;
+        private readonly IGenericRepository<Genre> genreRepository = genreRepository;
+
+        public async Task<IEnumerable<Game>> GetGamesAsync()
+        {
+            return await gameRepository.GetAllAsync();
+        }
+
+
+        public async Task<IEnumerable<Genre>> GetGenresAsync()
+        {
+            return await genreRepository.GetAllAsync();
+
+        }
+
+        public async Task<IEnumerable<ItemSubtype>> GetItemSubtypesAsync()
+        {
+            return await itemSubtypeRepository.GetAllAsync();
+
+        }
+    }
+}
---
+++ GamerMarketApp.Services.Data/Interfaces/IDropdownService.cs
@@ -0,0 +1,16 @@
+﻿using GamerMarketApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamerMarketApp.Services.Data.Interfaces
+{
+    public interface IDropdownService
+    {
+        Task<IEnumerable<Genre>> GetGenresAsync();
+        Task<IEnumerable<Game>> GetGamesAsync();
+        Task<IEnumerable<ItemSubtype>> GetItemSubtypesAsync();
+    }
+}
